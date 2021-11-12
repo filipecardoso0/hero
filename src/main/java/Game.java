8a8709672d04e40291d118e806aec8a26c352619
Filipe de Azevedo Cardoso <up202006409@up.pt>
2021-11-12 15:57:09 +0000

    Added unfinished character movement

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,14 +1,18 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import com.googlecode.lanterna.input.KeyStroke;
 import java.io.IOException;
 
 public class Game {
     private Screen screen;
+    private int x = 10;
+    private int y = 10;
 
 
     public Game(){
@@ -17,11 +21,8 @@ public class Game {
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
         Terminal terminal = terminalFactory.createTerminal();
         screen = new TerminalScreen(terminal);
-            System.out.println("Teste1");
 
         } catch (IOException e) {
-            System.out.println("Teste2");
-
             e.printStackTrace();
         }
     }
@@ -31,15 +32,7 @@ public class Game {
             screen.setCursorPosition(null); // we don't need a cursor
             screen.startScreen(); // screens must be started
             screen.doResizeIfNecessary(); // resize screen if necessary
-            System.out.println("Teste3");
-
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
-            screen.refresh();
-
         } catch (IOException e) {
-            System.out.println("Teste4");
-
             e.printStackTrace();
         }
     }
@@ -47,18 +40,49 @@ public class Game {
     public void draw() {
         try {
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')[0]);
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
             screen.refresh();
-            System.out.println("Teste5");
-
         } catch (IOException e) {
-            System.out.println("Teste6");
             e.printStackTrace();
         }
     }
 
-    public void run(){
+    public void run() {
         createTerminal();
         draw();
+        while (true){
+            try{
+            KeyStroke key = screen.readInput();
+            processKey(key);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowUp:
+                System.out.println("Primiu tecla -> Seta para cima");
+                screen.setCharacter(x+10, y, TextCharacter.fromCharacter('X')[0]);
+                break;
+            case ArrowDown:
+            {
+                System.out.println("Primiu tecla -> Seta para baixo");
+                screen.setCharacter(x-10, y, TextCharacter.fromCharacter('X')[0]);
+            }
+            case ArrowLeft:
+            {
+                System.out.println("Primiu tecla -> Seta para esquerda");
+                screen.setCharacter(x, y-10, TextCharacter.fromCharacter('X')[0]);
+            }
+            case ArrowRight:
+            {
+                System.out.println("Primiu tecla -> Seta para direita");
+                screen.setCharacter(x, y+10, TextCharacter.fromCharacter('X')[0]);
+            }
+            default:
+                break;
+        }
     }
 }
