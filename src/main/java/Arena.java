1e4b7e580270442c91363dd80c509a36a072937d
Filipe Cardoso <up202006409@up.pt>
2021-12-06 01:30:13 +0000

    Added coins and colision with hero

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -6,18 +6,22 @@ import com.googlecode.lanterna.screen.Screen;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class Arena {
     private int width, height;
     private Hero hero;
     private List<Wall> walls;
+    private List<Coin> coins;
 
     public Arena(int width, int height){
         this.width = width;
         this.height = height;
         this.hero = new Hero(10, 10);
         this.walls = createWalls();
+        this.coins = createCoins();
     }
 
     private List<Wall> createWalls() {
@@ -33,6 +37,30 @@ public class Arena {
         return walls;
     }
 
+    private List<Coin> createCoins() {
+        Random random = new Random();
+        ArrayList<Coin> coins = new ArrayList<>(); //Creates a new coins ArrayList object
+        for (int i = 0; i < 5; i++)
+            //Makes it impossible for the coin to spawn on top of the hero
+            if(!(((random.nextInt(width - 2) + 1) == hero.getPosition().getX()) && ((random.nextInt(height - 2) + 1 == hero.getPosition().getY())))){
+                coins.add(new Coin(random.nextInt(width - 2) + 1, random.nextInt(height - 2) + 1));
+            }
+            //Dont know how I can disable a coin from spawning on top of another one but they are not spawning on top of each other
+
+        return coins;
+    }
+
+    private List<Coin> retrieveCoins() {
+       for(Coin coin : coins){ //Searches for every coin position
+           if(coin.getPosition().equals(hero.getPosition())){
+               coins.remove(coin); //Removes the coin out of the array
+               break; //We only remove a coin at a time so it wont make sense to continue looping over it. Therefore we just break out of the loop
+           }
+       }
+       return coins;
+    }
+
+
     public void draw(TextGraphics graphics) throws IOException {
         //Draws the Arena Background
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
@@ -41,6 +69,11 @@ public class Arena {
         //Draws the Arena Walls
         for (Wall wall : walls) {
             wall.draw(graphics);
+        }
+
+        //Draws the Coins
+        for(Coin coin : coins){
+            coin.draw(graphics);
         }
 
 
@@ -55,6 +88,7 @@ public class Arena {
     private void moveHero(Position position) {
         if (canHeroMove(position)){
             hero.setPosition(position);
+            retrieveCoins();
         }
     }
 
