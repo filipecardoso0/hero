d78354cf032dfa129f54e949b7ad4005b29f1497
Filipe Cardoso <up202006409@up.pt>
2021-12-10 14:57:42 +0000

    testing

---
+++ src/test/groovy/HeroTesting.groovy
@@ -0,0 +1,7 @@
+import com.hero.filipecardoso0.Element
+
+class HeroTesting {
+    def "Removing dependency between Element classes and the lanterna library"(){
+
+    }
+}
---
+++ src/test/java/ElementTest.java
@@ -0,0 +1,13 @@
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.hero.filipecardoso0.GenericElement;
+import com.hero.filipecardoso0.StubGraphics;
+import com.hero.filipecardoso0.Element;
+import net.bytebuddy.description.type.TypeList;
+import org.junit.jupiter.api.Test;
+
+public class ElementTest {
+    @Test
+    public void elementTest(){
+
+    }
+}
